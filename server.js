530db6182fccc85d0bfca6213dfe111c8278668e
vikrant-pandey <vikrant.pandey@necsws.com>
2024-12-05 20:31:25 +0530

    add all rest apis

--- server.js
+++ server.js
@@ -23,9 +23,55 @@ const ItemSchema = new mongoose.Schema({
 });
 const Item = mongoose.model('Item', ItemSchema);
 
+const UserSchema = new mongoose.Schema({
+    userName: String,
+    password: String 
+  });
+const User = mongoose.model('User', UserSchema);
+
+const LRSchema = new mongoose.Schema({
+    lrNum: {
+        type: String,
+        unique: true,  
+        required: true
+    },
+    lrDate: Date,
+    party: String,
+    vehicalNo: String,
+    from: String,
+    to: String,
+    ownBy: String,
+    materialName: String,
+    loadnetWt: String,
+    unloadnetWt: String,
+    rate: String,
+    lrCheckedDate: Date,
+    reportingDate: Date,
+    unloadDate: Date,
+    shortage: String,
+    shortagePercent: String,
+    transportName: String,
+    advanceAmount: String,
+    advanceRemark: String,
+    dieselAmount: String,
+    shortagePermissible: String,
+    detention: String,
+    loadDate: Date,
+    dieselLtr: String,
+    remark: String,
+    others: String,
+    serviceBill: String,
+    fastTag: String,
+    secondRemark: String
+});
+
+const LR = mongoose.model('LR', LRSchema);
+
+
+
+
 // API Routes
 app.post('/items', async (req, res) => {
-    console.log(req.body);
   try {
     const newItem = new Item(req.body);
     await newItem.save();
@@ -42,6 +88,85 @@ app.get('/items', async (req, res) => {
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
+});
+
+app.get('/users', async (req, res) => {
+    try {
+      const users = await User.find();
+      res.status(200).json(users);
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+});
+
+app.get('/users/:userName', async (req, res) => {
+    const userName = req.params.userName;
+    try {
+      const users = await User.find();
+      const user = users.filter(res=>res.userName===userName);
+      if(!user) {
+        res.status(404).json({error : 'User Not Found'});
+      }
+      else{
+        res.status(200).json(user);
+      }
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+});
+
+app.post('/lrRecord', async (req, res) => {
+    const lrNumber = req.body.lrNum;
+    try {
+    const lrRecordId = await LR.findOne({ lrNum: lrNumber });
+      if (!lrRecordId) {
+        const lrRecord = new LR(req.body);
+        await lrRecord.save();
+        res.status(200).json(lrRecord);
+      }
+      else{
+        return res.status(404).json({ error: 'Already Record Exists With Same LR' });
+      }
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+  });
+  
+app.get('/lrRecord', async (req, res) => {
+    try {
+      const lrRecord = await LR.find();
+      res.status(200).json(lrRecord);
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+  });
+
+app.put('/lrRecord/:lrNum', async (req, res) => {
+    const lrNumber = req.params.lrNum;
+    try {
+      const lrRecord = await LR.findOne({ lrNum: lrNumber });
+      if (!lrRecord) {
+        return res.status(404).json({ error: 'Record not Found' });
+      }
+      Object.assign(lrRecord, req.body);
+      await lrRecord.save();
+      res.status(200).json(lrRecord);
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+});
+
+app.delete('/lrRecord/:lrNum', async (req, res) => {
+    const lrNumber = req.params.lrNum;
+    try {
+        const deletedRecord = await LR.findOneAndDelete({ lrNum: lrNumber });
+        if (!deletedRecord) {
+            return res.status(404).json({ error: 'Record not found' });
+        }
+        res.status(200).json({ message: 'Record deleted successfully', deletedRecord });
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
 });
 
 app.listen(5000, () => {
